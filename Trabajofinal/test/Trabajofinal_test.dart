f538eebdb75550c6807910f95528fe8d2cd92824
danielucsp <daniel.salinas@ucsp.edu.pe>
2015-06-24 20:21:50 -0500

    trabajo final

---
+++ Trabajofinal/test/Trabajofinal_test.dart
@@ -0,0 +1,14 @@
+// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+library Trabajofinal.test;
+
+import 'package:test/test.dart';
+
+import 'package:Trabajofinal/Trabajofinal.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
